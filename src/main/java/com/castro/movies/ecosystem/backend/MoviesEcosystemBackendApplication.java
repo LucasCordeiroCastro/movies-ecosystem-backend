cf598dde120017eea5672c66f9a6c34fd43b0303
Lucas <lucascordeirocastro@gmail.com>
2025-03-14 19:03:02 -0300

    Add project structure

---
+++ src/main/java/com/castro/movies/ecosystem/backend/MoviesEcosystemBackendApplication.java
@@ -0,0 +1,13 @@
+package com.castro.movies.ecosystem.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoviesEcosystemBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoviesEcosystemBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/castro/movies/ecosystem/backend/MoviesEcosystemBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.castro.movies.ecosystem.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MoviesEcosystemBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
